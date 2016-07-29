b636a498c6ca908145f61d2a5675f9a2ee112813
Samantha John <sam@gethopscotch.com>
2016-07-29 15:46:47 -0400

    Add a file/class to F1

---
+++ F1/File1.swift
@@ -0,0 +1,12 @@
+//
+//  File1.swift
+//  CocoapodsFrameworkBug
+//
+//  Created by Samantha John on 7/29/16.
+//  Copyright © 2016 Hopscotch. All rights reserved.
+//
+
+import Foundation
+class File1 {
+    
+}
