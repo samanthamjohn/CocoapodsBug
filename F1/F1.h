c1de6ff1e7d6bf0ced6a1adde667f2e98987c595
Samantha John <sam@gethopscotch.com>
2016-07-29 10:23:50 -0400

    Add f1 framework

---
+++ F1/F1.h
@@ -0,0 +1,19 @@
+//
+//  F1.h
+//  F1
+//
+//  Created by Samantha John on 7/29/16.
+//  Copyright © 2016 Hopscotch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for F1.
+FOUNDATION_EXPORT double F1VersionNumber;
+
+//! Project version string for F1.
+FOUNDATION_EXPORT const unsigned char F1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <F1/PublicHeader.h>
+
+
